a9c6b650bcc84ee101f8e0aa067e6dffbf69e1f7
William Vivas <williamvivasdev@gmail.com>
2019-06-17 13:49:31 -0400

    just made the schema file

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE 'burgers_db'
+USE 'burgers_db'
+CREATE TABLE 'Burger'(
+    ID int NOT NULL AUTO_INCREMENT,
+    Burger_Name varchar(255) NOT NULL,
+    Devoured boolean NOT NULL,
+
+)
---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO Burgers (ID, Burger_Name, Devoured)  
+VALUES (1, Bacon, True);
+
+INSERT INTO Burgers (ID, Burger_Name, Devoured)  
+VALUES (2, Vegan, True);
+
+INSERT INTO Burgers (ID, Burger_Name, Devoured)  
+VALUES (3, Black Bean, False);
+
